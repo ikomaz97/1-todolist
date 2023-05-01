e3dbf36066fd01dcb0dd83262edb7b6186c59b5a
ikomaz97 <ikomaz97@gmail.com>
2023-05-01 19:51:40 +0400

    01-02

--- src/App.tsx
+++ src/App.tsx
@@ -7,19 +7,14 @@ function App() {
     const shapka1 = "shapka1"
 
     const tasks1 = [
-        { id: 1, title: "HTML&CSS", isDone: true },
-        { id: 2, title: "JS", isDone: true },
-        { id: 3, title: "ReactJS", isDone: false }
+        {id: 1, title: "HTML&CSS", isDone: true},
+        {id: 2, title: "JS", isDone: true},
+        {id: 3, title: "ReactJS", isDone: false}
     ]
-    const tasks2 = [
-        { id: 1, title: "Hello world", isDone: true },
-        { id: 2, title: "I am Happy", isDone: false },
-        { id: 3, title: "Yo", isDone: false }
-    ]
+
     return (
         <div className="App">
-            <Todolist track1 = {shapka1} track2={100200} tasks={tasks1}/>
-            <Todolist track1 = {"shapka2"} tasks={tasks2}/>
+            <Todolist track1={shapka1} track2={100200} tasks={tasks1}/>
         </div>
     );
 }
