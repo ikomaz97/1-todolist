0b35c343571d75b9a46065024dadce8611c94039
ikomaz97 <ikomaz97@gmail.com>
2023-05-01 22:26:03 +0400

    finish-02

--- src/Todolist.tsx
+++ src/Todolist.tsx
@@ -1,53 +1,45 @@
 import React from 'react';
+import {ButtonNameType} from "./App";
 
-type TodolistPropsTitle = {
-    track1: string
-    track2?: number | boolean
-
-    tasks: Array<TaskType>
-}
 type TaskType = {
     id: number
     title: string
     isDone: boolean
 }
 
-export const Todolist = (props: TodolistPropsTitle) => {
-    return (
-        <div className="todolist">
-            <h3>{props.track1}</h3>
-            <div>
-                <input/>
-                <button>+</button>
-            </div>
-            <ul>
-                {props.tasks.map((el) => {
-                        debugger
-                        return (
-
-                            <li key={el.id}>
-                                <button onClick={() => {console.log(el.id)}}>X
-                                </button>
-                                <input type="checkbox" checked={el.isDone}/> <span>{el.title}</span>
-                                <span>{el.title}</span>
-                            </li>
-                        )
+type PropsType = {
+    title: string
+    tasks: Array<TaskType>
+    removeTask: (taskID: number, krasavchik: string) => void
+    filterTasks:(buttonName:ButtonNameType)=>void
+}
 
 
-                    }
-                )
-                }
+export function Todolist(props: PropsType) {
 
 
-            </ul>
-            <div>
-                <button>All</button>
-                <button>Active</button>
-                <button>Completed</button>
-            </div>
+    return <div>
+        <h3>{props.title}</h3>
+        <div>
+            <input/>
+            <button>+</button>
         </div>
-    );
-};
+        <ul>
+            {props.tasks.map((el, index) => {
+                return (
+                    <li key={el.id}>
+                        <button onClick={() => props.removeTask(el.id, 'Hellow krasavchik!')}>X</button>
+                        <input type="checkbox" checked={el.isDone}/>
+                        <span>{el.title}</span>
+                    </li>
+                )
+            })}
 
-export default Todolist;
-
+        </ul>
+        <div>
+            <button onClick={() =>props.filterTasks('All')}>All</button>
+            <button onClick={() =>props.filterTasks('Active')}>Active</button>
+            <button onClick={() =>props.filterTasks('Completed')}>Completed</button>
+        </div>
+    </div>
+}
